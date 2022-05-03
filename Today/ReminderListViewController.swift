51c22071a3c5c4610a90d4a16bf5a9cc14346db9
joensumin <soomis7@naver.com>
2022-05-04 08:44:16 +0900

    DataSource 설정

--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
+typealias DataSource = UICollectionViewDiffableDataSource<Int,String>
+
 class ReminderListViewController: UICollectionViewController {
+    
+    var dataSource: DataSource!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let layout = listLayout()
         collectionView.collectionViewLayout = layout
+        
+        let cellRegistration = UICollectionView.CellRegistration { (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
+            let reminder = Reminder.sampleData[indexPath.item]
+            var contentConfiguration = cell.defaultContentConfiguration()
+            contentConfiguration.text = reminder.title
+            cell.contentConfiguration = contentConfiguration
+        }
+        
+        dataSource = DataSource(collectionView: collectionView) { (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        }
     }
     
     
