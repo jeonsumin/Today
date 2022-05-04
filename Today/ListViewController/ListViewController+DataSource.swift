ae42f8c00e0c400a2d74417662dd2a7e48ce1b87
joensumin <soomis7@naver.com>
2022-05-04 11:12:35 +0900

    셀 정보  표시

--- Today/ListViewController/ListViewController+DataSource.swift
+++ Today/ListViewController/ListViewController+DataSource.swift
@@ -8,14 +8,32 @@
 import UIKit
 
 extension ReminderListViewController {
-    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String ){
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+    
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
         let reminder = Reminder.sampleData[indexPath.item]
         var contentConfiguration = cell.defaultContentConfiguration()
         contentConfiguration.text = reminder.title
-        //미리알림 기한 할당
         contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
-        // 글꼴 변경
         contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(forTextStyle: .caption1)
         cell.contentConfiguration = contentConfiguration
+        
+        var doneButtonConfiguration = doneButtonConfiguration(for: reminder)
+//        doneButtonConfiguration.tintColor = .todayListCellDoneButtonTint
+        cell.accessories = [ .customView(configuration: doneButtonConfiguration), .disclosureIndicator(displayed: .always) ]
+        
+        var backgroundConfiguration = UIBackgroundConfiguration.listGroupedCell()
+//        backgroundConfiguration.backgroundColor = .todayListCellBackground
+        cell.backgroundConfiguration = backgroundConfiguration
+    }
+    
+    private func doneButtonConfiguration(for reminder: Reminder) -> UICellAccessory.CustomViewConfiguration {
+        let symbolName = reminder.isComplete ? "circle.fill" : "circle"
+        let symbolConfiguration = UIImage.SymbolConfiguration(textStyle: .title1)
+        let image = UIImage(systemName: symbolName, withConfiguration: symbolConfiguration)
+        let button = UIButton()
+        button.setImage(image, for: .normal)
+        return UICellAccessory.CustomViewConfiguration(customView: button, placement: .leading(displayed: .always))
     }
 }
